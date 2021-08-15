8617611fb6bea9e7288ce7a50409644ee7e23428
Lee SeungYeon <81631536+SeungYeon-E@users.noreply.github.com>
2021-08-15 22:21:44 +0900

    image update

--- Dogtor/dogtor/Common/Common.swift
+++ Dogtor/dogtor/Common/Common.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class Common{
-    static let ipAddr = "172.30.1.21"
+    static let ipAddr = "127.0.0.1"
     static let jspPath = "http://\(ipAddr):8080/dogtor/"
     static let feedImagePath = "http://\(ipAddr):8080/dogtor/feedImage/"
     static let writerImagePath = "http://\(ipAddr):8080/dogtor/userImage/"
